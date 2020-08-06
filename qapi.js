e9bea82ef6ab68f9af6b1a8f483b97bdd380d313
Devzetsubou <66123340+Devzetsubou@users.noreply.github.com>
2020-08-06 13:30:22 +0300

    asd asd

---
+++ qapi.js
@@ -0,0 +1,24 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f2bd6c60d8c674ffe0f18ae",
+				"5f2bd6c60d8c674ffe0f18b0"
+			]
+		},
+		"5f2bd6c60d8c674ffe0f18ae": {
+			"id": "5f2bd6c60d8c674ffe0f18ae",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f2bd6c60d8c674ffe0f18b0": {
+			"id": "5f2bd6c60d8c674ffe0f18b0",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"projectType": "gatsby"
+}
