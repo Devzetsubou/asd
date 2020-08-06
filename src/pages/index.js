103ad1eaf96871d8932989ce7972d0c07dfb0015
Devzetsubou <66123340+Devzetsubou@users.noreply.github.com>
2020-08-06 14:58:46 +0300

    ффвыа

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,43 @@
 import React from "react";
 import theme from "theme";
-import { Theme } from "@quarkly/widgets";
+import { Theme, Text, Button } from "@quarkly/widgets";
+import { Section, Override } from "@quarkly/components";
 export default (() => {
-	return <Theme theme={theme}></Theme>;
+	return <Theme theme={theme}>
+		<Section
+			text-align="center"
+			background-color="--primary"
+			color="--light"
+			padding="100px 0"
+			sm-padding="40px 0"
+		>
+			<Text
+				as="h5"
+				font="--lead"
+				margin="10px 0 0 0"
+				text-transform="uppercase"
+				letter-spacing="5px"
+			>
+				Second to none
+			</Text>
+			<Text as="h1" font="--headline1" md-font="--headline2" margin="10px 0 0 0">
+				About Us
+			</Text>
+			<Text as="p" font="--lead" margin="40px 0 20px 0">
+				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.
+			</Text>
+		</Section>
+		<Section padding="100px 0" sm-padding="40px 0">
+			<Override slot="SectionContent" align-items="center" />
+			<Text as="h2" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
+				About Us
+			</Text>
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.
+			</Text>
+			<Button font="--lead" margin="20px">
+				Button
+			</Button>
+		</Section>
+	</Theme>;
 });
